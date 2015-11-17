da77ea1ab4efc4e76a2bb346a1599e62224fb22c
Colby Lemieux (cjl671) <cjl671@mail.usask.ca>
2015-11-17 16:48:33 -0600

    Added Berserker Enemy

---
+++ ProjectFiles/PupChaser/Assets/BerserkerThreatScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BerserkerThreatScript : MonoBehaviour {
+
+    public bool inVision;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            inVision = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D player)
+    {
+        if (player.tag == "Player")
+        {
+            inVision = false;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ProjectFiles/PupChaser/Assets/Scripts/BerserkerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BerserkerScript : MonoBehaviour {
+    public Vector3 targetPlayer;
+    public Vector3 targetSpawn;
+    public PlayerController Player;
+    public Rigidbody2D rBody;
+    public Animator anim;
+
+    delegate void MyDelagate();
+    MyDelagate beserkerAction;
+
+
+    public int healthPoints;
+    public float zombieSpeed;
+
+
+    //Function to handle collisions.
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "")
+        {
+        }
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        Player = FindObjectOfType<PlayerController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
