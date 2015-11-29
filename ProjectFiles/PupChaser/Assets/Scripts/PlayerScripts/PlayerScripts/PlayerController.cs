3d474a0db5937247064f7fe14ed28833e78f67db
Sebastian Guerrero Galvan (seg930) <seg930@mail.usask.ca>
2015-11-28 18:34:31 -0600

    fixed spider web

--- ProjectFiles/PupChaser/Assets/Scripts/PlayerScripts/PlayerScripts/PlayerController.cs
+++ ProjectFiles/PupChaser/Assets/Scripts/PlayerScripts/PlayerScripts/PlayerController.cs
@@ -91,7 +91,7 @@ public class PlayerController : MonoBehaviour {
 			invincible = true;
 			invincibilityCoolDown = 0.5f;
 		}
-		if (other.gameObject.name == "Spider Enemy(Clone)")
+		if (other.gameObject.name == "spider web(Clone)")
 		{
 			healthPoints = healthPoints--;
 			invincible = true;
