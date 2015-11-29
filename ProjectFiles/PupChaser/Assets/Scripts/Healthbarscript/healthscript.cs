968824940043118f4cb2091838036b54ff308df0
Sebastian Guerrero Galvan (seg930) <seg930@mail.usask.ca>
2015-11-29 15:42:32 -0600

    health bat

---
+++ ProjectFiles/PupChaser/Assets/Scripts/Healthbarscript/healthscript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class healthscript : MonoBehaviour {
+	
+	 //reference for slider
+	
+	void Start(){
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
